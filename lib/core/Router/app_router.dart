34e679df49dff5ebe1e36191c9ae9627f19f7864
Ahmed Elesawy <56431746+ahmedyosry433@users.noreply.github.com>
2025-02-01 14:46:44 +0200

    add routes

--- lib/core/Router/app_router.dart
+++ lib/core/Router/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:admin_dashboard/features/dashboard/presentation/screen/admin_screen.dart';
 import 'package:admin_dashboard/core/Router/routes.dart';
+import 'package:admin_dashboard/features/invoices/presentation/view/invesment_screen.dart';
+import 'package:admin_dashboard/features/wallet/presentation/view/wallet_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
@@ -9,6 +11,14 @@ class AppRouter {
       case Routes.dashboardScreen:
         return MaterialPageRoute(
           builder: (_) => const DashboardScreen(),
+        );
+      case Routes.investmentScreen:
+        return MaterialPageRoute(
+          builder: (_) => const InvestmentScreen(),
+        );
+      case Routes.walletScreen:
+        return MaterialPageRoute(
+          builder: (_) => const WalletScreen(),
         );
 
       default:
