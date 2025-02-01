1029049861d492e31083ddf42d00ab11b5373b97
Ahmed Elesawy <56431746+ahmedyosry433@users.noreply.github.com>
2025-02-01 10:22:41 +0200

    Create dashboard_app.dart

---
+++ lib/dashboard_app.dart
@@ -0,0 +1,21 @@
+import 'package:admin_dashboard/core/Router/app_router.dart';
+import 'package:admin_dashboard/core/Router/routes.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  final AppRouter appRouter;
+  const Dashboard({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.dashboardScreen,
+      onGenerateRoute: appRouter.onGenerateRoute,
+    );
+  }
+}
